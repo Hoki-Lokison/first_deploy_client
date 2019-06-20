45e7dd324166bf20b13cb3983bc0bddb444e71a2
Malia Adamson <sonictardis10@gmail.com>
2019-06-20 10:57:28 -0600

    update 2

---
+++ code.js
@@ -0,0 +1,49 @@
+var app = new Vue({
+  el: "#app",
+
+  data: {
+    greeting: "",
+    user:"",
+    age: 0,
+    date_created: 0,
+  },
+  created:function () {
+    this.loadName();
+  },
+
+
+  methods: {
+    loadName: function () {
+      var req_body = {
+        name: this.user,
+        age: this.age
+      };
+      fetch("http://localhost:8080/user", {
+        method: "POST",
+        headers:{
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(req_body)
+      }).then(function (response) {
+        response.json().then(function (data) {
+          app.user = data.name;
+          app.age = data.age;
+          app.date_created = data.created_on;
+          app.loadGreeting();
+        });
+      });
+    },
+      loadGreeting: function () {
+        fetch("http://localhost:8080/greeting").then(function (response) {
+        response.json().then(function ( data ) {
+          app.greeting = data.greeting;
+        });
+      });
+
+    },
+  },
+
+  computed: {
+
+  },
+});
